f68a74e4096c7f98b3008e788d184a0a0dde7171
ct1559 <e.cooper.taylor@gmail.com>
2022-05-25 16:21:37 -0400

    Add loop that reverses string input using arrays

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  strArr = str.split("");
+  reverseArr = [];
 
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    reverseArr.push(strArr[i]);
+  }
+  return reverseArr.join("");
 };
 
 // Do not edit below this line
