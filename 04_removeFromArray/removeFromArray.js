13dd53d6ebfca79b8d724a95370191455f383205
ct1559 <e.cooper.taylor@gmail.com>
2022-05-26 09:01:51 -0400

    Add loops to remove specified items in array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...theArgs) {
+  let newArr = [];
+  let addToNewArr = true;
+  for (const item of theArgs[0]) {
+    for (let i = 1; i < theArgs.length; i++) {
+      if (item !== theArgs[i]) {
+        continue;
+      } else {
+        addToNewArr = false;
+        break;
+      }
+    }
+    if (addToNewArr) {
+      newArr.push(item);
+    }
+    addToNewArr = true;
+  }
+  return newArr;
 };
 
 // Do not edit below this line
