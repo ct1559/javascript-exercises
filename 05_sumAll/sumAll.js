fe534e4fbb61c030abeb2e23b22a70ff5ead275f
ct1559 <e.cooper.taylor@gmail.com>
2022-05-26 10:42:02 -0400

    Add check for positive integer and loop to sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    num1 < 0 ||
+    typeof num2 !== "number" ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  } else {
+    let total = 0;
+    let max;
+    let min;
+    if (num1 > num2) {
+      max = num1;
+      min = num2;
+    } else {
+      max = num2;
+      min = num1;
+    }
 
+    for (let i = min; i <= max; i++) {
+      total += i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
