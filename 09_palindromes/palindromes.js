aa611f3890fa5d7bec879c52a2e08e717bf2a945
ct1559 <e.cooper.taylor@gmail.com>
2022-06-07 10:35:33 -0400

    Add logic to determine if string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let strArr = str.split("");
+  let newArr = [];
+  let regex = /(^[a-z])|(^[A-Z])/;
 
+  // Remove spaces and special characters
+  for (let i = 0; i < strArr.length; i++) {
+    if (regex.test(strArr[i])) {
+      newArr.push(strArr[i].toLowerCase());
+    }
+  }
+
+  let strForwards = newArr.join("");
+
+  // Creat Backwards Arr
+  let backwardsArr = [];
+  for (let i = newArr.length; i >= 0; i--) {
+    backwardsArr.push(newArr[i]);
+  }
+
+  let strBackwards = backwardsArr.join("");
+
+  console.log(strForwards);
+  console.log(strBackwards);
+
+  return strForwards === strBackwards;
 };
 
 // Do not edit below this line
