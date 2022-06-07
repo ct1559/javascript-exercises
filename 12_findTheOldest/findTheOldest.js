d3460f2fb18dd14a8d6a348e7e97e399430bfa77
ct1559 <e.cooper.taylor@gmail.com>
2022-06-07 13:01:27 -0400

    Add logic to find oldest person in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let currentOldestIndex = 0;
+  let currentOldestAge = 0;
+  let date = new Date();
+  date = date.getFullYear();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]["yearOfDeath"] === undefined) {
+      if (date - arr[i]["yearOfBirth"] > currentOldestAge) {
+        currentOldestIndex = i;
+        currentOldestAge = date - arr[i]["yearOfBirth"];
+      }
+    } else {
+      if (arr[i]["yearOfDeath"] - arr[i]["yearOfBirth"] > currentOldestAge) {
+        currentOldestIndex = i;
+        currentOldestAge = arr[i]["yearOfDeath"] - arr[i]["yearOfBirth"];
+      }
+    }
+  }
 
+  return arr[currentOldestIndex];
 };
 
 // Do not edit below this line
