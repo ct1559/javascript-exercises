55b632cb859cc5bd06f61d1ec3fab2dc4e0da7d9
ct1559 <e.cooper.taylor@gmail.com>
2022-06-07 08:58:07 -0400

    Add logic to all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let tempSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    tempSum += arr[i];
+  }
+  return tempSum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let tempProduct = 1;
+  for (let i = 0; i < arr.length; i++) {
+    tempProduct *= arr[i];
+  }
+  return tempProduct;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let tempProduct = 1;
+  for (let i = num; i > 0; i--) {
+    tempProduct *= i;
+  }
+  if (num === 0) {
+    return 1;
+  } else {
+    return tempProduct;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
